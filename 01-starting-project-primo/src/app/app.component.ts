291041e7536c8fd01dbfdd10439d6983a4a48986
elKryptos <154229030+elKryptos@users.noreply.github.com>
2024-08-28 22:32:00 +0200

    files

---
+++ 01-starting-project-primo/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-user';
+import { TasksComponent } from "./tasks/tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent, TasksComponent, NewTaskComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users = DUMMY_USERS
+  selectedUserId?: string
+
+  get selectedUser(){
+    return this.users.find((user) => user.id === this.selectedUserId)!
+  }  
+
+  onSelectUser(id: string){
+    this.selectedUserId = id
+  }
+}
